c374f4c10b5394468cd58a5abb983974a8c6ba00
elizerdim <elizerdim@gmail.com>
2023-07-28 12:12:31 +0300

    Add rotations for smaller degrees to the minute and hour hands

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const hourHand = document.querySelector(".hour-hand");
-const minHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setClock() {
     const currentTime = new Date();
-
+    
+    const currentSecond = currentTime.getSeconds();
+    secondHand.style.transform = `rotate(${currentSecond*6}deg)`;
+    
+    const currentMinute = currentTime.getMinutes();
+    minHand.style.transform = `rotate(${currentMinute*6 + currentSecond*(6 / 60)}deg)`;
+    
     const currentHour = currentTime.getHours();
-    hourHand.style.transform = `rotate(${currentHour*30}deg)`;
-
-    currentMinutes = currentTime.getMinutes();
-    minHand.style.transform = `rotate(${currentMinutes*6}deg)`;
-
-    currentSeconds = currentTime.getSeconds();
-    secondHand.style.transform = `rotate(${currentSeconds*6}deg)`;
+    hourHand.style.transform = `rotate(${currentHour*30 + currentMinute*(30 / 60)}deg)`;
 }
 
 setInterval(setClock, 1000);
